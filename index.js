b97c93f35d0e4f71c9ce796b1f1164de64fea2f1
th150233 <hippolyte.taquet@etu.univ-lehavre.fr>
2021-03-19 22:52:19 +0100

    y

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var Discord = require('discord.js');
+var fetch = require('node-fetch');
+
 var bot = new Discord.Client();
 var isReady = true;
 
@@ -24,15 +26,31 @@ bot.on('message', message => {
   }
 });
 
-bot.on('message', message => {
-  if (isReady && message.content === '!wt')
-  {
+bot.on('message', async message => {
+  if (message.author.bot) return;
+  if (isReady && message.content.indexOf("!wt") === 0){
   isReady = false;
-
-  message.channel.send('https://www.watch2gether.com/rooms/rdty570jqx5twlm7yd?lang=fr'); 
+  const args = "" + message.content.slice(3).trim().split(/ +/g);
+  fetch("https://w2g.tv/rooms/create.json", {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        "w2g_api_key": "4thd7mz1jflun254l4i18dsp9s5wif9rqwkcbh3u051vj0dr02jxvylc4gdcjihm",
+        "share": "" + args,
+        "bg_color": "#00ff00",
+        "bg_opacity": "50"
+    })
+})
+.then(response => response.json())
+.then(function (data) {
+  message.channel.send("Yep! \n https://w2g.tv/rooms/" + data.streamkey);
+});   
   isReady = true;
+
   }
 });
 
-
-bot.login(process.env.token);
+bot.login("NjM2NzQwNjE2NjM3OTA2OTcx.XbHRrw.VxLOZUa-4PQntqw3Sesz8OZLx4Y");
