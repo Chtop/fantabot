54ff8643b4043c8676b1657170aaaa420d00a712
th150233 <hippolyte.taquet@etu.univ-lehavre.fr>
2021-03-20 13:36:10 +0100

    OKOK

--- index.js
+++ index.js
@@ -4,26 +4,84 @@ var fetch = require('node-fetch');
 var bot = new Discord.Client();
 var isReady = true;
 
+var timeNuke;
+var nukeUsed;
+var useNuke;
+
 bot.on('message', message => {
   if (isReady && message.content === '!tg')
   {
   isReady = false;
-  let voiceChn = message.member.voiceChannel;
-
+  let voiceChn = message.member.voice.channel;
   if(!voiceChn){ 
     isReady = true;
     return message.channel.send('Viens vocal !'); 
   }
-  var voiceChannel = message.member.voiceChannel;
-  voiceChannel.join().then(connection =>
+  voiceChn.join().then(connection =>
   {
-     const dispatcher = connection.playFile('./Audio/tg.mp3');
-     dispatcher.on("end", end => {
-       voiceChannel.leave();
+     const dispatcher = connection.play('./Audio/tg.mp3');
+     dispatcher.on("finish", end => {
+      connection.disconnect();
        });
    }).catch(err => console.log(err));
    isReady = true;
   }
+});
+
+bot.on('message', message => {
+  if (isReady && message.content === '!nuke')
+  {
+  isReady = false;
+  let voiceChn = message.member.voice.channel;
+
+  // TODO : HashMap nukeUsed pour chaque serveur.
+
+  if(!voiceChn){ 
+    isReady = true; 
+    return 0; 
+  }
+  
+  
+  if (timeNuke === undefined){
+    timeNuke = Date.now();
+    nukeUsed = timeNuke - 1;
+  } else {
+    timeNuke = Date.now();
+  }
+
+  if (nukeUsed < timeNuke){
+    useNuke = true;
+    nukeUsed = timeNuke + 36000000;
+  } else {
+    message.channel.send("Prochaine nuke dispo dans : " + ((nukeUsed - timeNuke) /1000) + " secondes.");
+  }
+
+  
+  
+
+  if (useNuke){
+    useNuke = false;
+    voiceChn.join().then(connection =>
+      {
+         const dispatcher = connection.play('./Audio/nuke.mp3');
+         dispatcher.on("finish", end => {
+
+          voiceChn.members.forEach(member => {
+            member.voice.setChannel(null);
+          });
+
+          connection.disconnect();
+           });
+       }).catch(err => console.log(err));
+       isReady = true;
+      } else {
+        isReady = true;
+      }
+  }
+  
+  
+
+
 });
 
 bot.on('message', async message => {
@@ -40,7 +98,7 @@ bot.on('message', async message => {
     body: JSON.stringify({
         "w2g_api_key": process.env.tokenW2G,
         "share": "" + args,
-        "bg_color": "#00ff00",
+        "bg_color": "#000000",
         "bg_opacity": "50"
     })
 })
